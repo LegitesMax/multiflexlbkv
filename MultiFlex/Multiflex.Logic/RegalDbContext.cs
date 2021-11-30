8aec0092accbc86af1b3df626428e2ee9567fb4d
LegitesMax <55409186+LegitesMax@users.noreply.github.com>
2021-11-30 13:40:12 +0100

    added databse connection

---
+++ MultiFlex/Multiflex.Logic/RegalDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Multiflex.Logic
+{
+    public class RegalDbContext : DbContext
+    {
+        private static string ConnectionString => "Data Source=multiflex3.ddns.net,3306; Database=test; User Id=root; Passwort=admin";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
