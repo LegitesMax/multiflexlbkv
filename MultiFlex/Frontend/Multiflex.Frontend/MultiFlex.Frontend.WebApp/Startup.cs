d8fff777c27cc758985850027a54c0a5883b72b9
Markus Schwarz <herr-m-schwarz@gmx.at>
2021-12-05 17:22:22 +0100

    fix WebApp

--- MultiFlex/Frontend/Multiflex.Frontend/MultiFlex.Frontend.WebApp/Startup.cs
+++ MultiFlex/Frontend/Multiflex.Frontend/MultiFlex.Frontend.WebApp/Startup.cs
@@ -10,7 +10,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Multiflex.Webapp
+namespace Multiflex.Frontend.Webapp
 {
     public class Startup
     {
