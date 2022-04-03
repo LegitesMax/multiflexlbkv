f4e9566f89a48d3c12376e6e2909950e6d814a57
LegitesMax <55409186+LegitesMax@users.noreply.github.com>
2022-04-03 14:38:09 +0200

    fixed add/delete window

--- MultiFlex/Frontend/Multiflex.Frontend/MultiFlex.Frontend.WebApp/Controllers/LieferantenController.cs
+++ MultiFlex/Frontend/Multiflex.Frontend/MultiFlex.Frontend.WebApp/Controllers/LieferantenController.cs
@@ -1,10 +1,6 @@
-﻿using ElectronNET.API;
-using ElectronNET.API.Entities;
-using Microsoft.AspNetCore.Mvc;
-using Multiflex.Frontend.WebApp.Controllers.Commom;
-using Multiflex.Frontend.WebApp.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json.Linq;
-using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -13,67 +9,107 @@ namespace Multiflex.Frontend.WebApp.Controllers
 {
     public class LieferantenController : Controller
     {
-        public IActionResult Index()
+
+        private static List<Models.LieferantDto> lieferanten = new();
+
+        public async Task<ActionResult> Index()
         {
-            Electron.IpcMain.On("loadLieferanten", async (arg) =>
-            {
-                var mainWindow = Electron.WindowManager.BrowserWindows.First();
-                using var httpCliet = new HttpClient();
-                //Console.WriteLine("geht");
-                var requestlieferant = Task.Run(() =>
+            #region crap
+            //Electron.IpcMain.On("loadLieferanten", async (arg) =>
+            //{
+            //    var mainWindow = Electron.WindowManager.BrowserWindows.First();
+            //    using var httpCliet = new HttpClient();
+            //    //Console.WriteLine("geht");
+            //    var requestlieferant = Task.Run(() =>
+            //    {
+            //        return httpCliet.GetStringAsync("http://localhost:8080/lieferant");
+            //    });
+            //    var json = JArray.Parse(await requestlieferant);
+            //    //Console.WriteLine(requestlieferant);
+            //    Console.WriteLine("Json: Lieferant");
+            //    Console.WriteLine(json);
+
+            //    var items = System.Text.Json.JsonSerializer.Deserialize<Models.LieferantDto[]>(json.ToString());
+            //    lieferanten.AddRange(items);
+
+            //    Electron.IpcMain.Send(mainWindow, "getLieferant-reply", items);
+            //});
+
+            //Electron.IpcMain.On("open-add-window", async (arg) =>
+            //{
+            //    //var logic = new Logic();
+            //    //BrowserWindow win = await logic.AddWindow("/Lieferanten/Add");
+            //    //win.Show();
+            //    var path = "/Lieferanten/Add";
+            //    var options = new BrowserWindowOptions();
+            //    //{
+            //    //    Fullscreenable = false,
+            //    //    Minimizable = false,
+            //    //    SkipTaskbar = false,
+            //    //    EnableLargerThanScreen = false,
+            //    //    AutoHideMenuBar = true,
+            //    //    TitleBarStyle = TitleBarStyle.hiddenInset
+            //    //};
+            //    options.Show = false;
+            //    path = $"http://localhost:{BridgeSettings.WebPort}{path}";
+            //    var browserWindow = await Electron.WindowManager.CreateWindowAsync(options, path);
+            //    browserWindow.Show();
+            //});
+
+            //Electron.IpcMain.On("open-delete-window", async (arg) =>
+            //{
+            //    //var logic = new Logic();
+            //    //BrowserWindow win = await logic.AddWindow("/Lieferanten/Delete");
+            //    var path = "/Lieferanten/Delete";
+            //    var options = new BrowserWindowOptions
+            //    {
+            //        Fullscreenable = false,
+            //        Minimizable = false,
+            //        SkipTaskbar = false,
+            //        EnableLargerThanScreen = false,
+            //        AutoHideMenuBar = true,
+            //        TitleBarStyle = TitleBarStyle.hiddenInset
+            //    };
+            //    options.Show = false;
+            //    path = $"http://localhost:{BridgeSettings.WebPort}{path}";
+            //    var browserWindow = await Electron.WindowManager.CreateWindowAsync(options, path);
+            //    browserWindow.Show();
+            //    //win.Show();
+            //});
+            #endregion crap
+
+            using var httpCliet = new HttpClient();
+            var requestlieferant = Task.Run(() =>
                 {
                     return httpCliet.GetStringAsync("http://localhost:8080/lieferant");
                 });
-                //Console.WriteLine(requestlieferant);
-                var json = JArray.Parse(await requestlieferant);
-                Console.WriteLine("Json: Lieferant");
-                Console.WriteLine(json);
+            var json = JArray.Parse(await requestlieferant);
 
-                Electron.IpcMain.Send(mainWindow, "getLieferant-reply", json.ToString());
-            });
+            var items = System.Text.Json.JsonSerializer.Deserialize<Models.LieferantDto[]>(json.ToString());
+            lieferanten.AddRange(items);
 
-            Electron.IpcMain.On("open-add-window", async (arg) =>
-            {
-                //var logic = new Logic();
-                //BrowserWindow win = await logic.AddWindow("/Lieferanten/Add");
-                //win.Show();
-                var path = "/Lieferanten/Add";
-                var options = new BrowserWindowOptions();
-                //{
-                //    Fullscreenable = false,
-                //    Minimizable = false,
-                //    SkipTaskbar = false,
-                //    EnableLargerThanScreen = false,
-                //    AutoHideMenuBar = true,
-                //    TitleBarStyle = TitleBarStyle.hiddenInset
-                //};
-                options.Show = false;
-                path = $"http://localhost:{BridgeSettings.WebPort}{path}";
-                var browserWindow = await Electron.WindowManager.CreateWindowAsync(options, path);
-                browserWindow.Show();
-            });
+            var models = await GetAllAsync();
+            return View(models);
+        }
 
-            Electron.IpcMain.On("open-delete-window", async (arg) =>
-            {
-                //var logic = new Logic();
-                //BrowserWindow win = await logic.AddWindow("/Lieferanten/Delete");
-                var path = "/Lieferanten/Delete";
-                var options = new BrowserWindowOptions
-                {
-                    Fullscreenable = false,
-                    Minimizable = false,
-                    SkipTaskbar = false,
-                    EnableLargerThanScreen = false,
-                    AutoHideMenuBar = true,
-                    TitleBarStyle = TitleBarStyle.hiddenInset
-                };
-                options.Show = false;
-                path = $"http://localhost:{BridgeSettings.WebPort}{path}";
-                var browserWindow = await Electron.WindowManager.CreateWindowAsync(options, path);
-                browserWindow.Show();
-                //win.Show();
-            });
+        public async Task<ActionResult> DeleteAsync(int id)
+        {
+            var model = await GetByIdAsync(id);
+            return View(model);
+        }
 
+        public static Task<Models.LieferantDto[]> GetAllAsync()
+        {
+            return Task.Run(() => lieferanten.ToArray());
+        }
+
+        public static Task<Models.LieferantDto?> GetByIdAsync(int id)
+        {
+            return Task.Run(() => lieferanten.FirstOrDefault(s => s.id == id));
+        }
+
+        public ActionResult Add()
+        {
             return View();
         }
     }
--- MultiFlex/Frontend/Multiflex.Frontend/MultiFlex.Frontend.WebApp/Models/LieferantDto.cs
+++ MultiFlex/Frontend/Multiflex.Frontend/MultiFlex.Frontend.WebApp/Models/LieferantDto.cs
@@ -4,11 +4,11 @@ namespace Multiflex.Frontend.WebApp.Models
 {
     public class LieferantDto
     {
-        public int Id { get; set; }
-        public string name { get; set; }
-        public string weblink { get; set; }
+        public int id { get; set; }
         public int lieferzeit { get; set; }
-        public List<int> material_ids { get; set; }
+        public string name { get; set; }
+        public List<int> waren_ids { get; set; }
+        public string weblink { get; set; }
 
     }
 }
