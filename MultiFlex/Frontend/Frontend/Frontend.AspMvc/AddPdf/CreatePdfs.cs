1f572a11a0fe227a97f8334ed255d66405ef5d0d
fSchned <fabschian@gmail.com>
2022-06-19 12:36:41 +0200

    PDF Options

--- MultiFlex/Frontend/Frontend/Frontend.AspMvc/AddPdf/CreatePdfs.cs
+++ MultiFlex/Frontend/Frontend/Frontend.AspMvc/AddPdf/CreatePdfs.cs
@@ -1,6 +1,83 @@
-﻿namespace Frontend.AspMvc.AddPdf
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Diagnostics;
+using System.IO;
+using PdfSharp;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using PdfSharp.Drawing.Layout;
+
+
+namespace Frontend.AspMvc.AddPdf
 {
     public class CreatePdfs
     {
+        public static void ExportGraf_Click()
+        {
+            PdfDocument document = new PdfDocument();
+            document.Info.Title = "DataPDF";
+
+            for (int p = 0; p < 1; p++)
+            {
+                // Page Options
+                PdfPage pdfPage = document.AddPage();
+                pdfPage.Height = 842;//842
+                pdfPage.Width = 590;
+
+                // Get an XGraphics object for drawing
+                XGraphics graph = XGraphics.FromPdfPage(pdfPage);
+
+                // Text format
+                XStringFormat format = new XStringFormat();
+                format.LineAlignment = XLineAlignment.Near;
+                format.Alignment = XStringAlignment.Near;
+                var tf = new XTextFormatter(graph);
+
+                XFont fontParagraph = new XFont("Verdana", 8, XFontStyle.Regular);
+
+                // Row elements
+                int el1_width = 80;//Breite der Ersten Reihe
+                int el2_width = 380;//breite der zweiten Reihe
+
+                // page structure options
+                double lineHeight = 20;
+                int marginLeft = 20;
+                int marginTop = 20;
+
+                int el_height = 30;
+                int rect_height = 17;
+
+                int interLine_X_1 = 2;
+                int interLine_X_2 = 2 * interLine_X_1;
+
+                int offSetX_1 = el1_width;
+                int offSetX_2 = el1_width + el2_width;
+
+                XSolidBrush rect_style1 = new XSolidBrush(XColors.LightGray);
+                XSolidBrush rect_style2 = new XSolidBrush(XColors.DarkGreen);
+                XSolidBrush rect_style3 = new XSolidBrush(XColors.Red);
+
+                
+
+            //File.s
+            const string filename = "C:\\Users\\fabsc\\Desktop\\test\\HelloWorld.pdf";
+            document.Save(filename);
+
+            //byte[] bytes = null;
+            //using (MemoryStream stream = new MemoryStream())
+            //{
+            //    document.Save(stream, true);
+            //    bytes = stream.ToArray();
+            //}
+
+            //SendFileToResponse(bytes, "HelloWorld_test.pdf");
+
+        }
     }
 }
