24c5c401988f340f7eaa2fb83bd3bcc7e3051b21
fSchned <fabschian@gmail.com>
2022-06-19 22:24:49 +0200

    Revert "Edit Index"
    This reverts commit 4cda9afa781805706d3e1fc73e332acdb6b3f8a0.

--- MultiFlex/Frontend/Frontend/Frontend.Logic/Entities/Catigory/Product.cs
+++ MultiFlex/Frontend/Frontend/Frontend.Logic/Entities/Catigory/Product.cs
@@ -8,8 +8,9 @@ namespace Frontend.Logic.Entities.Catigory
         public string name { get; set; }
         public int minValue { get; set; }
         public List<int> material_ids { get; set; }
-        public string colorName { get; set; }
-        public string colorCode { get; set; }
+        public int size_id { get; set; }
+        public int color_id { get; set; }
+        public int category_id { get; set; }
         public int value { get; set; }
     }
 }
