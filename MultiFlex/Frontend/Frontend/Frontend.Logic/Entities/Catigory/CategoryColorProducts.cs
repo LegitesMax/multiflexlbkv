8c4c48b01532b3976e9939d702743c66552c7b8b
fSchned <fabschian@gmail.com>
2022-06-19 22:25:09 +0200

    Revert "Create DTo s"
    This reverts commit c5796fc4049250fbac2564ded4018a64dffef1f2.

--- MultiFlex/Frontend/Frontend/Frontend.Logic/Entities/Catigory/CategoryColorProducts.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Frontend.AspMvc.Models
-{
-    public class CategoryColorProducts
-    {
-        public Category category { get; set; }
-        public List<Product> products { get; set; }
-    }
-}
