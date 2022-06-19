c5796fc4049250fbac2564ded4018a64dffef1f2
fSchned <fabschian@gmail.com>
2022-06-19 19:40:27 +0200

    Create DTo s

---
+++ MultiFlex/Frontend/Frontend/Frontend.Logic/Entities/Catigory/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace Frontend.AspMvc.Models
+{
+    public class Category
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string acronym { get; set; }
+        public List<int> product_ids { get; set; }
+    }
+}
---
+++ MultiFlex/Frontend/Frontend/Frontend.Logic/Entities/Catigory/CategoryColorProducts.cs
@@ -0,0 +1,8 @@
+﻿namespace Frontend.AspMvc.Models
+{
+    public class CategoryColorProducts
+    {
+        public Category category { get; set; }
+        public List<Product> products { get; set; }
+    }
+}
